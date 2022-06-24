f23a7cb7e6251277cad22fba2dc5d0f76e39d303
Eozef <www.github.com/Eozef.com>
2022-06-24 23:05:15 +1000

    Adding Start button

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,26 +1,25 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getQuote } from "./start/api/api";
 
 export const Start = () => {
   const [quote, setQuete] = useState(0);
-  const isMounted = useRef(false);
+  // const [intervalId, setIntervalId] = useState();
 
+  //   useEffect(() => {
+  //     if (isMounted.current) {
+  //       doSomething(data);
+  //     } else {
+  //       isMounted.current = true;
+  //     }
+  //   }, [data]);
+  // }
 
-  useEffect(() => {
-    if (isMounted.current) {
-      doSomething(data);
-    } else {
-      isMounted.current = true;
-    }
-  }, [data]);
-}
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      fetchData();
-    }, 5000);
-    return () => clearInterval(intervalId);
-  }, [quote, setQuete]);
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     fetchData();
+  //   }, 5000);
+  //   return () => clearInterval(intervalId);
+  // }, [quote, setQuete]);
 
   const fetchData = async () => {
     try {
@@ -30,14 +29,27 @@ export const Start = () => {
       throw Error("e", e);
     }
   };
+  let intervalId;
+  function toggleInterval() {
+    if (!intervalId) {
+      intervalId = setInterval(() => {
+        fetchData();
+      }, 1000);
+    }
+  }
+
+  function stopInterval() {
+    clearInterval(intervalId);
+    console.log("hello", intervalId);
+    intervalId = null;
+  }
 
   return (
     <div>
       <p>{quote.quoteAuthor}</p>
       <h2>"{quote.quoteText}"</h2>
-      <button disabled={quote} onClick={fetchData}>
-        New quote
-      </button>
+      <button onClick={toggleInterval}>Start</button>
+      <button onClick={stopInterval}>Stop</button>
     </div>
   );
 };
